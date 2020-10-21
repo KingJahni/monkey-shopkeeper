cbc987f08d4c8e5c5989304103a936afd60a1171
KingJahni <73185308+KingJahni@users.noreply.github.com>
2020-10-21 10:35:54 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let coins = 12
+let apple_price = 2
+let apple_quantity = 2
+let strawberry_price = 3
+let strawberry_quantity = 6
+let chicken_leg_price = 7
+let chicken_leg_quantity = 3
+scene.setBackgroundColor(5)
+game.splash("You have " + coins + " coins from the start")
+coins = coins + apple_price * apple_quantity
+game.splash("You have " + coins + " coins now after selling the apples")
+game.splash("Welcome")
